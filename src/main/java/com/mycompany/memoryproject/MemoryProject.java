cf5d4011263c2a0857d3c24e96bd629acbd9a5e9
RsolFlacko <rsolano061@gmail.com>
2022-03-28 21:01:28 -0600

    First commit

---
+++ src/main/java/com/mycompany/memoryproject/MemoryProject.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.memoryproject;
+
+/**
+ *
+ * @author Dell
+ */
+public class MemoryProject {
+
+    @SuppressWarnings("deprecation")
+    public static void main(String[] args) {
+        InicioJuego inicio = new InicioJuego();
+        inicio.setVisible(true);
+    }
+}
